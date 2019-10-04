5f37712939ffd7cea64afaa9a63c081a7525e02f
Zhuk Sergey <seregazhuk88@gmail.com>
2019-10-04 13:38:19 +0300

    Remove debug from tests

--- tests/Helper/WatcherRunner.php
+++ tests/Helper/WatcherRunner.php
@@ -9,13 +9,8 @@ final class WatcherRunner
     public function run($scriptToRun, array $arguments = []): Process
     {
         $arguments = array_merge($arguments, ['--delay', 0.25]);
-        //if (!isset($arguments['config'])) {
-        //    $arguments = array_merge($arguments, ['--delay', 0.25]);
-        //}
         $process = new Process(array_merge(['./php-watcher', $scriptToRun], $arguments));
-        $process->start(function ($a, $g) {
-            //echo $g . PHP_EOL;
-        });
+        $process->start();
 
         return $process;
     }
