dbd815338dfcecab7eb8469725f7fb3460c9ab7e
seregazhuk88@gmail.com <seregazhuk88@gmail.com>
2020-06-13 23:29:34 +0300

    Fix fixtures for testing

--- tests/Feature/Helper/Filesystem.php
+++ tests/Feature/Helper/Filesystem.php
@@ -75,7 +75,7 @@ CODE;
 
     public static function fixturesDir(): string
     {
-        return self::FIXTURES_DIR;
+        return str_replace('tests/', '', self::FIXTURES_DIR);
     }
 
     private static function buildFilePath(string $filename): string
